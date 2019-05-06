1035bdc2418b1d33a8e15aad255b565764e0239e
snehal <snehalwarke@gmail.com>
2019-05-06 23:57:50 +0530

    Second commit

--- src/main/java/pages/OnLineCatalogPage.java
+++ src/main/java/pages/OnLineCatalogPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import base.PredefinedActions;
 import contant.ConstantPath;
@@ -12,7 +13,12 @@ public class OnLineCatalogPage extends PredefinedActions {
 
 
 	private static OnLineCatalogPage onLineCatalogPage;
-	PropertyFileOperation propOperation;
+	private PropertyFileOperation propOperation;
+	public enum ITEM{
+		TENTS, BACKPACKS,BOOTS, SUNGLASS,SOCKS,SHORTS,
+	}
+	
+	
 	private OnLineCatalogPage() throws IOException
 	{
 		 propOperation = new PropertyFileOperation(ConstantPath.ONLINECATALOGPAGEPATH);
@@ -51,34 +57,106 @@ public class OnLineCatalogPage extends PredefinedActions {
 	
 	public void clickSubmitBtn()
 	{
-		 click(propOperation.getValue("paceAnOrderBtn"));
+		 click(propOperation.getValue("placeAnOrderBtn"));
 	}
 	
-	public void orderQuantityDemoTent(int quantity)
+	public OnLineCatalogPage orderQuantityDemoTent(int quantity)
 	{
-		setText(propOperation.getValue(""), String.valueOf(quantity), true);
+		setText(propOperation.getValue("QTY_TENTS"),String.valueOf(quantity),true);
+		return this;
 	}
+	
+	
+	public OnLineCatalogPage orderQuantityFrameBackPack(int quantity)
+	{
+		setText(propOperation.getValue("QTY_BACKPACKS"),String.valueOf(quantity),true);
+		return this;
+	}
+	
+	
 	public void setQuantity(OnlineCatelogPOJO onlineCatelogPOJO) {
 		// TODO Auto-generated method stub
 		if(onlineCatelogPOJO.getDemoTent()!=null&&!onlineCatelogPOJO.getDemoTent().equals(""))
 			setText(propOperation.getValue("QYT_TENTS"), onlineCatelogPOJO.getDemoTent(), true);
 		
 		if(onlineCatelogPOJO.getFrameBackPack()!=null&&!onlineCatelogPOJO.getFrameBackPack().equals(""))
-			setText(propOperation.getValue("QTY_BACKPACKS"), onlineCatelogPOJO.getDemoTent(), true);
+			setText(propOperation.getValue("QTY_BACKPACKS"), onlineCatelogPOJO.getFrameBackPack(), true);
 		
 		if(onlineCatelogPOJO.getSunGlasses()!=null&&!onlineCatelogPOJO.getSunGlasses().equals(""))
-			setText(propOperation.getValue("QYT_TENTS"), onlineCatelogPOJO.getDemoTent(), true);
-		
-		if(onlineCatelogPOJO.getHikingBoot()!=null&&!onlineCatelogPOJO.getHikingBoot().equals(""))
-			setText(propOperation.getValue("QYT_TENTS"), onlineCatelogPOJO.getDemoTent(), true);
+			setText(propOperation.getValue("QTY_GLASSES"), onlineCatelogPOJO.getSunGlasses(), true);
 		
 		if(onlineCatelogPOJO.getPaddedSocks()!=null&&!onlineCatelogPOJO.getPaddedSocks().equals(""))
-			setText(propOperation.getValue("QYT_TENTS"), onlineCatelogPOJO.getDemoTent(), true);
+			setText(propOperation.getValue("QTY_SOCKS"), onlineCatelogPOJO.getPaddedSocks(), true);
+		
+		if(onlineCatelogPOJO.getHikingBoot()!=null&&!onlineCatelogPOJO.getHikingBoot().equals(""))
+			setText(propOperation.getValue("QTY_BOOTS"), onlineCatelogPOJO.getHikingBoot(), true);
 		
 		if(onlineCatelogPOJO.getBackCountryShorts()!=null&&!onlineCatelogPOJO.getBackCountryShorts().equals(""))
-			setText(propOperation.getValue("QYT_TENTS"), onlineCatelogPOJO.getDemoTent(), true);
+			setText(propOperation.getValue("QTY_SHORTS"), onlineCatelogPOJO.getBackCountryShorts(), true);
 		
 		
 	}
+	
+	public OnlineCatelogPOJO getQuantity()
+	{
+		OnlineCatelogPOJO onlineCatelogPOJO=new OnlineCatelogPOJO();
+		onlineCatelogPOJO.setDemoTent(getText(propOperation.getValue("QYT_TENTS")));
+		onlineCatelogPOJO.setFrameBackPack(getText(propOperation.getValue("QTY_BACKPACKS")));;
+		
+		
+		return onlineCatelogPOJO;
+	}
 
+
+
+	public String getQuantity(ITEM item)
+	{
+		
+		if(item==ITEM.TENTS)
+			return getAttributeValue(propOperation.getValue("QYT_TENTS"), "value");
+		if(item==ITEM.BACKPACKS)
+			return getAttributeValue(propOperation.getValue("QTY_BACKPACKS"), "value");
+		if(item==ITEM.SUNGLASS)
+			return getAttributeValue(propOperation.getValue("QTY_GLASSES"), "value");
+		if(item==ITEM.SOCKS)
+			return getAttributeValue(propOperation.getValue("QTY_SOCKS"), "value");
+		if(item==ITEM.BOOTS)
+			return getAttributeValue(propOperation.getValue("QTY_BOOTS"), "value");
+		if(item==ITEM.SHORTS)
+			return getAttributeValue(propOperation.getValue("QTY_SHORTS"), "value");
+			
+		
+		return null;
+		
+	}
+	
+	public boolean verifyPlaceAnOrderAlertPresent()
+	{
+		return verifyAlertPresent();
+	}
+	
+	public String getTextPlaceAnOrderAlert()
+	{
+		return handleAccept();
+	}
+
+	
+	public ArrayList<Integer> getItemIds()
+	{
+		ArrayList<Integer> indexNumber = new ArrayList<Integer>();
+		int totalRow = getTotalRowCount(propOperation.getValue("itemTable")); // 8
+		int index = Integer.parseInt(getText(propOperation.getValue("itemNumber").replace("%x%", "2")))+1; // initial value 
+		
+		for(int i=3;i<totalRow;i++) // 8 [2-7]
+		{
+			int currentIndex = Integer.parseInt(getText(propOperation.getValue("itemNumber").replace("%x%", String.valueOf(i))));
+			if(currentIndex != (index+1))
+			{
+				indexNumber.add(currentIndex);
+			}
+			index++;
+		}
+		return indexNumber;
+	}
+	
 }
